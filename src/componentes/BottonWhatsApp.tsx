1057f9d0206f8512f1155d4e5fb7ba261a350d45
Pablo Martins <pablomartins1515@hotmail.com>
2022-08-18 12:41:14 -0300

    Update BottonWhatsApp.tsx

--- src/componentes/BottonWhatsApp.tsx
+++ src/componentes/BottonWhatsApp.tsx
@@ -4,7 +4,7 @@ function BottomWhatsApp() {
 
 
   return (
-    <a href="https://contate.me/BuscarDados">
+    <a href="https://api.whatsapp.com/send?phone=5599984633422&text=OI.%20Tudo%20bom%3F%20Gostaria%20de%20buscar%20informa%C3%B5es%20sobre%20o%20meu%20ve%C3%ADculo%20atrav%C3%A9s%20da%20placa%20e%20Chassi.">
       <div>
         <svg
           xmlns="http://www.w3.org/2000/svg"
